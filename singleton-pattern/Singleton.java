5bf652d2d9b9fa6f8146fe6bab5f36cf5cca0ef9
Junier Damian <onieldp@gmail.com>
2018-11-12 10:39:43 -0400

    Singleton pattern

---
+++ singleton-pattern/Singleton.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+    private static Singleton instance = new Singleton();
+    public String str;
+    private Singleton() {}
+    
+    public static Singleton getSingleInstance() { 
+        return instance;
+    }
+}
